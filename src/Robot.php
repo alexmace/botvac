e234547da69418285e929a2bdac2447893cab75b
Alex Mace <alexmace@polaris-solutions.co.uk>
2016-12-11 21:35:32 +0000

    Implemented the functions that had tests implemented so far.

--- src/Robot.php
+++ src/Robot.php
@@ -20,8 +20,33 @@ class Robot
     // documentation.
     const SERVICE_EASY_CONNECT      = 'easyConnect';
 
+    const STATE_INVALID = 0;
+    const STATE_IDLE    = 1;
+    const STATE_BUSY    = 2;
+    const STATE_PAUSED  = 3;
+    const STATE_ERROR   = 4;
+
+    const ACTION_INVALID                = 0;
+    const ACTION_HOUSE_CLEANING         = 1;
+    const ACTION_SPOT_CLEANING          = 2;
+    const ACTION_MANUAL_CLEANING        = 3;
+    const ACTION_DOCKING                = 4;
+    const ACTION_USER_MENU_ACTIVE       = 5;
+    const ACTION_SUSPENDED_CLEANING     = 6;
+    const ACTION_UPDATING               = 7;
+    const ACTION_COPYING_LOGS           = 8;
+    const ACTION_RECOVERING_LOCATION    = 9;
+    const ACTION_IEC_TEST               = 10;
+
     private $api;
     private $apiVersion;
+    private $isCharging;
+    private $isDocked;
+    private $isScheduleEnabled;
+    private $hasSeenDock;
+    private $batteryCharge;
+    private $state;
+    private $action;
     private $servicesAvailable = [
         self::SERVICE_FIND_ME           => false,
         self::SERVICE_GENERAL_INFO      => false,
@@ -49,11 +74,54 @@ class Robot
         return $this->servicesAvailable[$service];
     }
 
+    public function isCharging()
+    {
+        return $this->isCharging;
+    }
+
+    public function isDocked()
+    {
+        return $this->isDocked;
+    }
+
+    public function isScheduleEnabled()
+    {
+        return $this->isScheduleEnabled;
+    }
+    public function hasSeenDock()
+    {
+        return $this->hasSeenDock;
+    }
+
+    public function getBatteryCharge()
+    {
+        return $this->batteryCharge;
+    }
+
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    public function getAction()
+    {
+        return $this->action;
+    }
+
     private function processStateResponse($stateResponse)
     {
         if ( ! ($stateResponse instanceof stdClass)
             || !isset($stateResponse->availableServices)
+            || !isset($stateResponse->state)
+            || !isset($stateResponse->action)
             || ! ($stateResponse->availableServices instanceof stdClass)
+            || !isset($stateResponse->details)
+            || ! ($stateResponse->details instanceof stdClass)
+            || !isset($stateResponse->details->isCharging)
+            || !isset($stateResponse->details->isDocked)
+            || !isset($stateResponse->details->isScheduleEnabled)
+            || !isset($stateResponse->details->dockHasBeenSeen)
+            || !isset($stateResponse->details->charge)
         ) {
             throw new Exception('Unable to process state response received');
         }
@@ -72,6 +140,15 @@ class Robot
             // reported for every service the robot supports.
             $this->apiVersion = $version;
         }
+
+        $this->isCharging = $stateResponse->details->isCharging;
+        $this->isDocked = $stateResponse->details->isDocked;
+        $this->isScheduleEnabled = $stateResponse->details->isScheduleEnabled;
+        $this->hasSeenDock = $stateResponse->details->dockHasBeenSeen;
+        $this->batteryCharge = $stateResponse->details->charge;
+
+        $this->state = $stateResponse->state;
+        $this->action = $stateResponse->action;
 
     }
 }
